5754dc4660fd6f81b9768fa38e8f8428000c08aa
Yohann Bénédic <6747553+yobeonline@users.noreply.github.com>
2025-09-09 06:26:18 +0200

    Ajout d'un test (#2)

--- test/test_xml.cpp
+++ test/test_xml.cpp
@@ -4,6 +4,7 @@
 #include <doctest/doctest.h>
 
 #include <sstream>
+#include <vector>
 
 using namespace io1::xml;
 
@@ -74,14 +75,62 @@ TEST_CASE("Empty utf-8 and standalone xml")
   CHECK(lines[1] == R"(<root />)");
 }
 
-TEST_CASE("Empty wit attributes")
+TEST_CASE("Empty with attributes")
 {
   std::stringstream ss;
-  doc(ss, "root") << attr("lang", "en&fr");
+  doc(ss, "root") << attr("lang", "en");
 
   auto const lines = getlines(ss);
   CHECK(lines.size() == 2);
   CHECK(lines[0] == R"(<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>)");
-  CHECK(lines[1] == R"(<root lang="en&amp;fr" />)");
+  CHECK(lines[1] == R"(<root lang="en" />)");
 }
 
+TEST_CASE("Attributes escaping: quotes, less-than and greater-than") {
+  std::stringstream ss;
+  doc(ss, "root") << attr("v", R"("a&b<c>d")");
+  auto const lines = getlines(ss);
+  CHECK(lines.size() == 2);
+  CHECK(lines[1] == R"(<root v="&quot;a&amp;b&lt;c&gt;d&quot;" />)");
+}
+
+TEST_CASE("Multiple attributes with escaping") {
+  std::stringstream ss;
+  doc(ss, "root") << attr("a", "1&2") << attr("b", "x<y");
+  auto const lines = getlines(ss);
+  CHECK(lines.size() == 2);
+  // If attribute order is defined as insertion-order:
+  CHECK(lines[1] == R"(<root a="1&amp;2" b="x&lt;y" />)");
+}
+
+TEST_CASE("Root with single child tag")
+{
+  std::stringstream ss;
+  {
+    auto d = doc(ss, "root");
+    d << tag("foo");
+  }
+
+  auto const lines = getlines(ss);
+  CHECK(lines.size() == 4);
+  CHECK(lines[0] == R"(<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>)");
+  CHECK(lines[1] == R"(<root>)");
+  CHECK(lines[2] == R"(  <foo />)");
+  CHECK(lines[3] == R"(</root>)");
+}
+
+TEST_CASE("Root with two child tags preserves order") {
+  std::stringstream ss;
+  {
+    auto d = doc(ss, "root");
+    d << tag("foo");
+    d << tag("bar");
+  }
+  auto const lines = getlines(ss);
+  CHECK(lines.size() == 5);
+  CHECK(lines[0] == R"(<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>)");
+  CHECK(lines[1] == R"(<root>)");
+  CHECK(lines[2] == R"(  <foo />)");
+  CHECK(lines[3] == R"(  <bar />)");
+  CHECK(lines[4] == R"(</root>)");
+}
